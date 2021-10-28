08dc27c622eaee9369041bbb509e851542fb96cc
Matheus Vieira <matheus.vieira.2332@gmail.com>
2021-10-27 23:18:55 -0300

    completed

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">
+  <div class="block" v-if="showBlock" @click="stopTimer">
       click me
   </div>
 </template>
@@ -9,14 +9,27 @@ export default {
     props: ['delay'],
     data() {
         return {
-            showBlock: false
+            showBlock: false,
+            timer: null,
+            reactionTime: 0 // the result is in ms
         }
     },
     mounted() {
-
         setTimeout(() => {
             this.showBlock = true
+            this.startTimer()
         }, this.delay)
+    },
+    methods: {   //control the time of the timer
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            this.$emit('end', this.reactionTime) // end of game
+        }
     }
 }
 </script>
